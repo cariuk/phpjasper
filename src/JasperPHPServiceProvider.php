5e0b3ecdce781b474dd260d9692100bf1d6a758e
Hade Mopilie <hademopilie@gmail.com>
2023-11-15 22:05:35 +0800

    feat: fixing composer instalation

---
+++ src/JasperPHPServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PHPJasper;
+
+use Illuminate\Support\ServiceProvider;
+class JasperPHPServiceProvider extends ServiceProvider
+{
+    /**
+     * Booting the package.
+     */
+    public function boot()
+    {
+
+    }
+}
